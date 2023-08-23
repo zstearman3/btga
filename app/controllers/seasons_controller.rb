8473726f4ca026bebbd6f647e36748d8c59d1289
Zach Stearman <zstearman@hey.com>
2023-08-23 05:13:22 +0000

    sort schedule

--- app/controllers/seasons_controller.rb
+++ app/controllers/seasons_controller.rb
@@ -3,7 +3,7 @@ class SeasonsController < ApplicationController
     @season = Season.includes(events: [{tournament: :tournament_level}, :course]).find_by(year: params[:year])
     @previous_season = Season.find_by(year: @season.year - 1)
     @next_season = Season.find_by(year: @season.year + 1)
-    @events = @season.events
+    @events = @season.events.order(start_date: :asc)
     @admin = params[:admin]
   end
 end
